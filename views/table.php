fd0bbb446f99091c5b262a72053a2c07fc316c87
Nicolas Hurtubise <316k@legtux.org>
2014-01-20 01:48:44 -0500

    Adds useful code to the "table" sample view

--- views/table.php
+++ views/table.php
@@ -1,14 +1,36 @@
 <h2><?php echo $table ?></h2>
 <table>
     <tr>
-        <?php foreach($columns as $column): ?>
-            <th><?php echo $column ?></th>
+        <?php foreach($columns as $alias => $column): ?>
+            <th><?php echo is_int($alias) ? $column : $alias ?></th>
         <?php endforeach ?>
     </tr>
     <?php foreach($data as $row => $values): ?>
         <tr>
             <?php foreach($columns as $column): ?>
-                <td><?php echo $values[$column]['column'] ?></td>
+            <?php
+                $value = $values[$column]['column'];
+                $type = $values[$column]['type']; 
+                if($type == "enum") {
+                    $options = $values[$column]['options'];
+                }
+                ?>
+                <td>
+                    <?php // Null ? ?>
+                    <?php if(in_array($type, array('string', 'int', 'float'))): ?>
+                        <input type="text" value="<?php echo $value ?>" />
+                    <?php elseif($type == 'enum'): ?>
+                        <select>
+                            <?php foreach($options as $option): ?>
+                                <option<?php echo ($option == $value ? ' selected' : '')?>><?php echo $option ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    <?php elseif($type == 'boolean'): ?>
+                        <input type="checkbox" <?php echo $value ? 'checked' : '' ?>/>
+                    <?php elseif($type == 'readonly'): ?>
+                        <p><?php echo $value ?></p>
+                    <?php endif; ?>
+                </td>
             <?php endforeach ?>
         </tr>
     <?php endforeach ?>
